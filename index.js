1187ed32b0edd48df7dd87edc0a583f1d73fcfa5
Enio Santinelli Filho <eniofilho76@gmail.com>
2022-05-22 14:41:13 -0300

    desafio

---
+++ index.js
@@ -0,0 +1,3 @@
+const primeiroNome = "seu nome";
+const diaDoMes = 12;
+const hojeFezSol = true;
---
+++ index.ts
@@ -0,0 +1,20 @@
+const primeiroNome: string = "seu nome";
+const diaDoMes: number = 12;
+const hojeFezSol: boolean = true;
+
+
+
+type Cachorrro = {
+  nome: string,
+   idade: number,
+  raça: boolean,
+  grandePorte: boolean,
+  donos: string
+}
+
+interface Animais {
+  tipo: string,
+  bipede: boolean,
+  asas: boolean,
+  região: string,
+}
